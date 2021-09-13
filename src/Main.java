0048b46e0a9f649d3d3b1460ba2a66f55b654545
Aidan Border <planes4u@outlook.com>
2021-09-13 12:38:38 -0400

    Trying to get an algorithm down for decimal to binary conversion

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Converting 874 from decimal to binary");
-        System.out.println("1. Convert from decimal to");
+        int num = 874;
+
+        String binaryNum = "";
+
+        while (num > 0) {
+            num = num / 2;
+            binaryNum =  num % 2 + binaryNum;
+            System.out.println(num);
+            System.out.println(binaryNum);
+        }
 
     }
 }
