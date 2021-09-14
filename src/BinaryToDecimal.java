7d1f0bbbfb50fa127c1d12927daee2a6bfd78c36
Aidan Border <planes4u@outlook.com>
2021-09-14 17:47:50 -0400

    *insert Charles Leclerc "I am stupid" here*
    Got decimal to binary and vice versa working

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -1,5 +1,20 @@
 public class BinaryToDecimal {
 	public static void main(String[] args) {
+		int binaryNum = 1000101;
+		int decimalNumber = 0;
+		int i = 0;
+		int temp = 0;
+		while (true) {
+			if (binaryNum == 0) {
+				break;
+			} else {
+				temp = binaryNum % 10;
+				decimalNumber += temp * Math.pow(2, i);
+				binaryNum = binaryNum / 10;
+				i++;
 
+			}
+		}
+		System.out.println(decimalNumber);
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Converting 874 from decimal to binary");
-        int num = 437;
+        int num = 420;
 
         String binaryNum = "";
 
-        while (num > 0) {
-            num = num / 2;
+         do {
             binaryNum =  num % 2 + binaryNum;
+            num = num / 2;
             System.out.println(num);
             System.out.println(binaryNum);
-        }
+        } while (num > 0);
 
     }
 }
